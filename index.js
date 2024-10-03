c647c500ced8d23666f34606791241a318783a49
juliadragons <juliaf@hot.ee>
2024-10-03 11:34:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function tervita(req, res) {
     console.log(req.query)
     res.end(`
         <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    h1 {
+                        color:red;
+                    }
+                </style>
+            </head>
             <body>
                 <h1>Tere, ${req.query.nimi} </h1>
             </body>
@@ -13,5 +23,30 @@ function tervita(req, res) {
         `)
 }
 
+function parim(req, res) {
+    res.end(`
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    span {
+                        color:violet;
+                        font-size: 30ox;
+                        font-weight: bold;
+                    }
+                </style>
+            </head>
+            <body>
+                <span>${req.query.nimi} on parim koduloom maailmas!</span>
+            </body>
+        </html>
+        `)
+}
+
+
+
 app.get('/tervitus', tervita)
+app.get('/parim', parim)
 app.listen(3000)
